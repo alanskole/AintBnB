c0c1010e6da4612aa0f330115fb19a51c7f52196
Alan Dana Omar <aland@hiof.no>
2021-02-08 13:30:36 +0100

    api work

--- AintBnB/ViewModels/BookingViewModel.cs
+++ AintBnB/ViewModels/BookingViewModel.cs
@@ -1,12 +1,146 @@
-﻿using System;
+﻿using AintBnB.Core.Models;
+using AintBnB.Helpers;
+using Newtonsoft.Json;
+using System.Net.Http;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System;
+using AintBnB.Services;
+using System.Text;
 
 namespace AintBnB.ViewModels
 {
-    public class BookingViewModel
+    public class BookingViewModel : Observable
     {
+        private string _startDate;
+        private int _bookerId;
+        private int _night;
+        private int _accommodationId;
+        private HttpClientProvider _clientProvider = new HttpClientProvider();
+        private string _uri;
+        private string _uniquePartOfUri;
+        private Booking _booking;
+        private int _bookingId;
+
+        public string StartDate
+        {
+            get { return _startDate; }
+            set
+            {
+                _startDate = value;
+                NotifyPropertyChanged("StartDate");
+            }
+        }
+
+        public int BookerId
+        {
+            get { return _bookerId; }
+            set
+            {
+                _bookerId = value;
+                NotifyPropertyChanged("BookerId");
+            }
+        }
+
+        public int Nights
+        {
+            get { return _night; }
+            set
+            {
+                _night = value;
+                NotifyPropertyChanged("Nights");
+            }
+        }
+
+        public int AccommodationId
+        {
+            get { return _accommodationId; }
+            set
+            {
+                _accommodationId = value;
+                NotifyPropertyChanged("AccommodationId");
+            }
+        }
+
+        public Booking Booking
+        {
+            get { return _booking; }
+            set
+            {
+                _booking = value;
+                NotifyPropertyChanged("Booking");
+            }
+        }
+
+        public int BookingId
+        {
+            get {return _bookingId; }
+            set
+            {
+                _bookingId = value;
+                NotifyPropertyChanged("BookingId");
+            }
+        }
+
+        public BookingViewModel()
+        {
+            _clientProvider.ControllerPartOfUri = "api/booking/";
+            _uri = _clientProvider.LocalHostAddress + _clientProvider.LocalHostPort + _clientProvider.ControllerPartOfUri;
+        }
+
+        public async Task BookAccommodation()
+        {
+            _uniquePartOfUri = StartDate + "/" + BookerId.ToString() + "/" + Nights.ToString() + "/" + AccommodationId.ToString();
+
+            HttpResponseMessage response = await _clientProvider.client.GetAsync(new Uri(_uri + _uniquePartOfUri));
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonBooking = await response.Content.ReadAsStringAsync();
+                Booking = JsonConvert.DeserializeObject<Booking>(jsonBooking);
+                NotifyPropertyChanged("Booking");
+            }
+            else
+                throw new ArgumentException(response.Content.ReadAsStringAsync().Result);
+        }
+
+        public async Task GetABooking()
+        {
+            _uniquePartOfUri = BookingId.ToString();
+
+
+            HttpResponseMessage response = await _clientProvider.client.GetAsync(new Uri(_uri + _uniquePartOfUri));
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonBooking = await response.Content.ReadAsStringAsync();
+                Booking = JsonConvert.DeserializeObject<Booking>(jsonBooking);
+                NotifyPropertyChanged("Booking");
+            }
+            else
+                throw new ArgumentException(response.Content.ReadAsStringAsync().Result);
+        }
+
+        public async Task<List<Booking>> GetAllBookings()
+        {
+            List<Booking> all = new List<Booking>();
+
+            HttpResponseMessage response = await _clientProvider.client.GetAsync(new Uri(_uri));
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonBookings = await response.Content.ReadAsStringAsync();
+                all = JsonConvert.DeserializeObject<List<Booking>>(jsonBookings);
+                return all;
+            }
+            else
+                throw new ArgumentException(response.Content.ReadAsStringAsync().Result);
+        }
+
+        public async Task DeleteABooking()
+        {
+            _uniquePartOfUri = BookingId.ToString();
+
+            HttpResponseMessage response = await _clientProvider.client.DeleteAsync(new Uri(_uri + _uniquePartOfUri));
+            if (!response.IsSuccessStatusCode)
+                throw new ArgumentException(response.Content.ReadAsStringAsync().Result);
+        }
     }
 }
---
+++ AintBnB/Views/SearchPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using AintBnB.ViewModels;
+using AintBnB.Core.Models;
+using Windows.UI.Popups;
+
+namespace AintBnB.Views
+{
+    public sealed partial class SearchPage : Page
+    {
+        public AccommodationViewModel ViewModel { get; } = new AccommodationViewModel();
+        public EuropeViewModel EuropeViewModel { get; } = new EuropeViewModel();
+        public BookingViewModel BookingViewModel { get; } = new BookingViewModel();
+        public AuthenticationViewModel AuthenticationViewModel { get; } = new AuthenticationViewModel();
+
+        public SearchPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ComboBoxCountries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EuropeViewModel.Country = ComboBoxCountries.SelectedValue.ToString();
+
+            ComboBoxCities.ItemsSource = await EuropeViewModel.GetAllCitiesOfACountry();
+        }
+
+        private void ComboBoxCities_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EuropeViewModel.City = ComboBoxCities.SelectedValue.ToString();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComboBoxCountries.ItemsSource = await EuropeViewModel.GetAllCountriesInEurope();
+        }
+
+        private async void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = listView.SelectedIndex;
+
+            List<Accommodation> availableAccs = await ViewModel.GetAllAccommodations();
+
+            Accommodation acc = availableAccs[index];
+
+            var container = new StackPanel();
+
+            var contentDialog = new ContentDialog
+            {
+                Title = "Book Accommodation",
+                Content = container,
+                PrimaryButtonText = "Book",
+                CloseButtonText = "Cancel"
+            };
+
+            ContentDialogResult result = await contentDialog.ShowAsync();
+            if (result == ContentDialogResult.Primary)
+            {
+                BookingViewModel.StartDate = ViewModel.FromDate;
+                BookingViewModel.BookerId = await AuthenticationViewModel.IdOfLoggedInUser();
+                BookingViewModel.Nights = int.Parse(nights.Text);
+                BookingViewModel.AccommodationId = acc.Id;
+                try
+                {
+                    await BookingViewModel.BookAccommodation();
+                    await new MessageDialog("Booking successful!").ShowAsync();
+                }
+                catch (Exception ex)
+                {
+                    await new MessageDialog(ex.Message).ShowAsync();
+                }
+            }
+
+        }
+
+        private async void Button_Click_Search(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Accommodation.Address.Country = EuropeViewModel.Country;
+
+            ViewModel.Accommodation.Address.City = EuropeViewModel.City;
+
+            try
+            {
+                listView.ItemsSource = await ViewModel.GetAvailable();
+
+            }
+            catch (Exception ex)
+            {
+                await new MessageDialog(ex.Message).ShowAsync();
+            }
+        }
+
+        private void MyDatePicker_DateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
+        {
+            var date = MyDatePicker.Date;
+
+            DateTime dt = date.Value.DateTime;
+
+            ViewModel.FromDate = dt.ToString("yyyy-MM-dd");
+        }
+    }
+}
