d5819325e4a8f173630e7e2a11e28d441a02a182
Alan Dana Omar <aland@hiof.no>
2021-02-17 04:51:38 +0100

    removed unused using at top of file

--- AintBnB.BusinessLogic/Repository/AccommodationRepository.cs
+++ AintBnB.BusinessLogic/Repository/AccommodationRepository.cs
@@ -3,7 +3,6 @@ using AintBnB.Database.DbCtx;
 using AintBnB.BusinessLogic.DependencyProviderFactory;
 using System.Collections.Generic;
 using System.Linq;
-using System;
 using Microsoft.EntityFrameworkCore;
 
 namespace AintBnB.BusinessLogic.Repository
