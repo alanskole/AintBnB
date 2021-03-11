ef6cc2bd44ce3f1fa4a643b7d049a1f9384cdc9e
Alan Dana Omar <aland@hiof.no>
2021-03-11 05:42:01 +0100

    corrected regex

--- AintBnB.BusinessLogic/Helpers/Regexp.cs
+++ AintBnB.BusinessLogic/Helpers/Regexp.cs
@@ -4,9 +4,9 @@ namespace AintBnB.BusinessLogic.Helpers
 {
     public static class Regexp
     {
-        public static Regex onlyLettersOneSpaceOrDash = new Regex(@"^(?=.{2,}$)([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff]+([\s-]?[A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff]+){0,})$");
-        public static Regex onlyLettersNumbersOneSpaceOrDash = new Regex(@"^(?=.{2,}$)([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff0-9]([\s-]?[A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff0-9]+)*)$");
+        public static Regex onlyLettersOneSpaceOrDash = new Regex(@"^(?=.{2,}$)([\p{Lu}\p{Ll}]+([\s-]?[\p{Lu}\p{Ll}]+))$");
+        public static Regex onlyLettersNumbersOneSpaceOrDash = new Regex(@"^(?=.{2,}$)([\p{Lu}\p{Ll}0-9]([\s-]?[\p{Lu}\p{Ll}0-9]+)*)$");
         public static Regex onlyNumbersFollowedByAnOptionalLetter = new Regex(@"^[1-9]+[0-9]*[A-Za-z]?$");
-        public static Regex zipCodeFormatsOfTheWorld = new Regex(@"^(?=.{2,10}$)([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff0-9]([\s-]?[A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff0-9]+)*)$");
+        public static Regex zipCodeFormatsOfTheWorld = new Regex(@"^(?=.{2,10}$)([\p{Lu}\p{Ll}0-9]([\s-]?[\p{Lu}\p{Ll}0-9]+)*)$");
     }
 }
